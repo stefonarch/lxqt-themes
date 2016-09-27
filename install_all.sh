70d377912656d9a6423749f0655bdac1d98d560a
stefonarch <stef.binde@riseup.net>
2016-09-27 22:14:36 +0200

    updated screens, added install script

---
+++ install_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#check if root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+#check for git
+hash git &> /dev/null
+if [ $? -eq 1 ]; then
+    echo >&2 "Please install git to use this script"
+	 exit 1
+else
+    echo "Found git - ok"
+fi
+	cd /tmp
+for i in \
+	archlinux-dark \
+	silver \
+	black-red \
+	black-green \
+	ice
+
+do
+	echo "Downloading $i theme ........."
+	git clone -q https://github.com/stefonarch/$i
+	cp -av $i /usr/share/lxqt/themes/
+	echo "LXQt theme '$i' installed"
+	echo "+++++++++++++"
+#cleanup
+	rm -rf $i
+done
+
+echo "Install finished"
